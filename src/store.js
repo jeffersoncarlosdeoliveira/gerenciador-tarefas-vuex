0c8f00b5d1ddcf8c2545fd70a3ae88fdfdd68657
Jefferson <jeffersoncarlosdeoliveira@outlook.com>
2020-11-05 12:12:09 -0300

    First Commit

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        contador: 0,
+        tarefas: [
+            { id: 1, titulo: 'Aprender Vue', concluido: true },
+            { id: 2, titulo: 'Aprender Vue Router', concluido: true },
+            { id: 3, titulo: 'Aprender Vuex', concluido: false }
+        ]
+    },
+    getters:{
+        tarefasConcluidas:(state) =>{
+            return state.tarefas.filter(t => t.concluido)
+        },
+        tarefasAFazer:(state) =>{
+            return state.tarefas.filter(t => !t.concluido)
+        },
+        totalTarefasConcluidas: (state,getters) => getters.tarefasConcluidas.length
+    }
+})
